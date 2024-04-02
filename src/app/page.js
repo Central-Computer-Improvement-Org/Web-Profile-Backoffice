f049f1f8d1b268cad3dd477d9138e4a818bb2556
kevinirawand <kevinid07@gmail.com>
2024-04-02 14:47:30 +0700

    test redirect

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,9 @@ import React, { useEffect } from 'react';
 import OurRedirect from './utils/OurRedirect';
 
 export default function Index() {
-   const router = useRouter();
+   // const router = useRouter();
 
-   OurRedirect('./dashboard');
+   // OurRedirect('./login');
 
    // useEffect(() => {
    //    // router.replace('/login'); // Mengarahkan pengguna dari / ke /login
