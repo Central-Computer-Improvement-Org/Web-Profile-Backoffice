142dd8569381cc13b401586789680dfec1895170
aryafirmansyah2 <aryafirmansyah200404@example.com>
2024-04-04 18:50:07 +0700

    RBLD-53 | Buat simple mockup response | Mockup Respone

--- src/app/api/division/route.js
+++ src/app/api/division/route.js
@@ -3,7 +3,56 @@ export async function GET(request) {
     code: 200,
     status: 'SUCCESS',
     recordsTotal: 0,
-    data: [{}],
+    data: [
+      {
+        id: 'CCI-WEBD',
+        name: 'Web Development',
+        description:
+          'Web Developer adalah perancang sebuah website / www. Web Developer merancang sebuah website dari awal sekali, menciptakan coding dsb, atau bisa juga seorang Web Developer mengembangkan sebuah website yang sudha ada. Web Developer bisa bekerja secara terikat dengan perusahaan, atau menjadi pekerja lepas (freelancer).',
+        logoUrl:
+          'https://img.freepik.com/free-vector/gradient-code-logo-with-tagline_23-2148811020.jpg?size=338&ext=jpg&ga=GA1.1.1224184972.1711756800&semt=ais',
+      },
+      {
+        id: 'CCI-WEBD',
+        name: 'Web Development',
+        description:
+          'Web Developer adalah perancang sebuah website / www. Web Developer merancang sebuah website dari awal sekali, menciptakan coding dsb, atau bisa juga seorang Web Developer mengembangkan sebuah website yang sudha ada. Web Developer bisa bekerja secara terikat dengan perusahaan, atau menjadi pekerja lepas (freelancer).',
+        logoUrl:
+          'https://img.freepik.com/free-vector/gradient-code-logo-with-tagline_23-2148811020.jpg?size=338&ext=jpg&ga=GA1.1.1224184972.1711756800&semt=ais',
+      },
+      {
+        id: 'CCI-WEBD',
+        name: 'Web Development',
+        description:
+          'Web Developer adalah perancang sebuah website / www. Web Developer merancang sebuah website dari awal sekali, menciptakan coding dsb, atau bisa juga seorang Web Developer mengembangkan sebuah website yang sudha ada. Web Developer bisa bekerja secara terikat dengan perusahaan, atau menjadi pekerja lepas (freelancer).',
+        logoUrl:
+          'https://img.freepik.com/free-vector/gradient-code-logo-with-tagline_23-2148811020.jpg?size=338&ext=jpg&ga=GA1.1.1224184972.1711756800&semt=ais',
+      },
+      {
+        id: 'CCI-WEBD',
+        name: 'Web Development',
+        description:
+          'Web Developer adalah perancang sebuah website / www. Web Developer merancang sebuah website dari awal sekali, menciptakan coding dsb, atau bisa juga seorang Web Developer mengembangkan sebuah website yang sudha ada. Web Developer bisa bekerja secara terikat dengan perusahaan, atau menjadi pekerja lepas (freelancer).',
+        logoUrl:
+          'https://img.freepik.com/free-vector/gradient-code-logo-with-tagline_23-2148811020.jpg?size=338&ext=jpg&ga=GA1.1.1224184972.1711756800&semt=ais',
+      },
+      {
+        id: 'CCI-WEBD',
+        name: 'Web Development',
+        description:
+          'Web Developer adalah perancang sebuah website / www. Web Developer merancang sebuah website dari awal sekali, menciptakan coding dsb, atau bisa juga seorang Web Developer mengembangkan sebuah website yang sudha ada. Web Developer bisa bekerja secara terikat dengan perusahaan, atau menjadi pekerja lepas (freelancer).',
+        logoUrl:
+          'https://img.freepik.com/free-vector/gradient-code-logo-with-tagline_23-2148811020.jpg?size=338&ext=jpg&ga=GA1.1.1224184972.1711756800&semt=ais',
+      },
+      {
+        id: 'CCI-WEBD',
+        name: 'Web Development',
+        description:
+          'Web Developer adalah perancang sebuah website / www. Web Developer merancang sebuah website dari awal sekali, menciptakan coding dsb, atau bisa juga seorang Web Developer mengembangkan sebuah website yang sudha ada. Web Developer bisa bekerja secara terikat dengan perusahaan, atau menjadi pekerja lepas (freelancer).',
+        logoUrl:
+          'https://img.freepik.com/free-vector/gradient-code-logo-with-tagline_23-2148811020.jpg?size=338&ext=jpg&ga=GA1.1.1224184972.1711756800&semt=ais',
+      },
+    ],
     error: null,
   };
   return new Response(JSON.stringify(responseData));
---
+++ src/app/api/projectById/route.js
@@ -0,0 +1,15 @@
+export async function GET(request) {
+  const responseData = {
+    code: 200,
+    status: 'SUCCESS',
+    recordsTotal: 0,
+    data: {
+      id: 'PRJ-12345',
+      issuer: 'Penghargaan Web Development Gemastik',
+      productionUrl: 'https://github.com/name/example',
+      repositoryUrl: 'https://github.com/name/example',
+    },
+    error: null,
+  };
+  return new Response(JSON.stringify(responseData));
+}
