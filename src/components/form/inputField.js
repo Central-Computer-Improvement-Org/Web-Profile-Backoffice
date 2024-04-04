dcc2ecf31f20cb23d6d28818a1b01178a74901fd
dapraws <darrelprawira26@gmail.com>
2024-04-04 23:43:05 +0700

    Update Component | InputField

--- src/components/form/inputField.js
+++ src/components/form/inputField.js
@@ -9,6 +9,7 @@ const InputField = ({
   value,
   onChange,
   placeholder,
+  multiple = false,
   required = false,
   icon = null,
   label = null,
@@ -33,6 +34,7 @@ const InputField = ({
           value={value}
           onChange={onChange}
           placeholder={placeholder}
+          multiple={multiple}
           required={required}
           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
         />
