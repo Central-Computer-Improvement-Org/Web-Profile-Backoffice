53bb9251711b441ac02c3f5971d02bfd84b16bd3
dapraws <darrelprawira26@gmail.com>
2024-04-06 19:29:11 +0700

    RBLD-47 | CRUD UI News FIX | News

--- src/app/(backoffice)/news/page.js
+++ src/app/(backoffice)/news/page.js
@@ -12,6 +12,10 @@ import DefaultLink from '@/components/link/defaultLink';
 import DefaultButton from '@/components/button/defaultButton';
 import HeadTitle from '@/components/headTitle';
 import DefaultTable from '@/components/table/defaultTable';
+import Link from 'next/link';
+import request from '@/app/utils/request';
+import Pagination from '@/components/pagination';
+import ListNews from '@/components/listTable/listNews';
 
 export default function NewsPage() {
   // Gunakan huruf besar untuk nama fungsi komponen
@@ -27,8 +31,8 @@ export default function NewsPage() {
   ];
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3000/api/news')
+    request
+      .get('/news')
       .then(function (response) {
         setDatas(response.data.data);
         setLoading(false);
@@ -94,46 +98,7 @@ export default function NewsPage() {
               )
             )}
           </DefaultTable>
-          <div className="flex items-center mb-4 sm:mb-0 py-5">
-            <a
-              href="#"
-              className="inline-flex justify-center p-1 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 "
-            >
-              <svg
-                className="w-7 h-7"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </a>
-            <a
-              href="#"
-              className="inline-flex justify-center p-1 mr-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100"
-            >
-              <svg
-                className="w-7 h-7"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </a>
-            <span className="text-sm font-normal text-gray-500 ">
-              Showing <span className="font-semibold text-gray-900 ">1-20</span>{' '}
-              of <span className="font-semibold text-gray-900 ">2290</span>
-            </span>
-          </div>
+          <Pagination />
         </div>
       )}
     </div>
