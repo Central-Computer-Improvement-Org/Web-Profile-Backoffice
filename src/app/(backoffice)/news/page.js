7ee85b9baa39305fe39fffb4a559d3165e5ba07d
dapraws <darrelprawira26@gmail.com>
2024-04-06 19:29:11 +0700

    RBLD-47 | CRUD UI News FIX | News

--- src/app/(backoffice)/news/page.js
+++ src/app/(backoffice)/news/page.js
@@ -15,6 +15,7 @@ import DefaultTable from '@/components/table/defaultTable';
 import Link from 'next/link';
 import request from '@/app/utils/request';
 import Pagination from '@/components/pagination';
+import ListNews from '@/components/listTable/listNews';
 
 export default function NewsPage() {
   // Gunakan huruf besar untuk nama fungsi komponen
