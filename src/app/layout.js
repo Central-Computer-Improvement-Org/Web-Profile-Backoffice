875486e848abf149cc578ea38e0717b2a4a57568
kevinirawand <kevinid07@gmail.com>
2024-05-06 23:19:15 +0700

    test meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
    return (
       <html lang="en">
+         <head>
+            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+         </head>
          <body className={inter.className}>
             <Suspense
                fallback={
