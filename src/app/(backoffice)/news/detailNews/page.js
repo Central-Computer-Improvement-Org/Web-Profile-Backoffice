8a019690689375d72bcde423b385041205ad000a
Aryfrmnsyh_ <aryafirmansyah200404@gmail.com>
2024-11-16 10:42:32 +0700

    RBLD-187 | Fix Bug News | News

--- src/app/(backoffice)/news/detailNews/page.js
+++ src/app/(backoffice)/news/detailNews/page.js
@@ -1,23 +1,27 @@
-/* eslint-disable @next/next/no-img-element */
 "use client";
+
 import React, { useEffect, useState, useContext } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import Image from "next/image";
-import { Editor, EditorState, ContentState, convertToRaw } from "draft-js";
-import draftToHtml from "draftjs-to-html";
-import htmlToDraft from "html-to-draftjs";
-
+import dynamic from "next/dynamic";
 import { StateContext } from "@/app/(backoffice)/state";
 import request from "@/app/utils/request";
 import DefaultLink from "@/components/link/defaultLink";
 import LogoNotfound from "/public/assets/icon/notfound.svg";
+import { default as htmlToDraft } from "html-to-draftjs";
+import { EditorState, ContentState, convertToRaw } from "draft-js";
+
+const EditorNoSSR = dynamic(
+  () => import("draft-js").then((mod) => mod.Editor),
+  {
+    ssr: false,
+  }
+);
 
 function DetailNewsPage() {
   const searchParams = useSearchParams();
   const router = useRouter();
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
+  const [editorState, setEditorState] = useState(null);
   const id = searchParams.get("id");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -33,7 +37,7 @@ function DetailNewsPage() {
     }
     request
       .get(`/news?id=${id}`)
-      .then(function (response) {
+      .then((response) => {
         const data = response.data.data;
         setTitle(data.title);
         setDescription(data.description);
@@ -41,33 +45,32 @@ function DetailNewsPage() {
         setDetailsMediaUrl(data.detailNewsMedia);
         setNewsName(data.title);
         setNewsId(data.id);
-        setLoading(false); // Setelah data dimuat, atur loading menjadi false
+        setLoading(false);
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.log(error);
-        setLoading(false); // Jika terjadi kesalahan, tetap atur loading menjadi false
+        setLoading(false);
       });
   }, [id, router, setNewsName, setNewsId]);
 
-  // Konversi HTML ke Draft.js saat komponen dimuat
   useEffect(() => {
-    const blocksFromHtml = htmlToDraft(description);
-    const { contentBlocks, entityMap } = blocksFromHtml;
-    const contentState = ContentState.createFromBlockArray(
-      contentBlocks,
-      entityMap
-    );
-    const editorState = EditorState.createWithContent(contentState);
-    setEditorState(editorState);
+    if (typeof window !== "undefined" && description) {
+      const blocksFromHtml = htmlToDraft(description);
+      const contentState = ContentState.createFromBlockArray(
+        blocksFromHtml.contentBlocks,
+        blocksFromHtml.entityMap
+      );
+      const editorState = EditorState.createWithContent(contentState);
+      setEditorState(editorState);
+    }
   }, [description]);
 
-  // Konversi EditorState ke HTML untuk ditampilkan
-  const onEditorStateChange = (editorState) => {
-    setEditorState(editorState);
+  const onEditorStateChange = (newEditorState) => {
+    setEditorState(newEditorState);
     const currentHtml = draftToHtml(
-      convertToRaw(editorState.getCurrentContent())
+      convertToRaw(newEditorState.getCurrentContent())
     );
-    console.log(currentHtml); // Ini adalah HTML yang diperbarui
+    console.log(currentHtml);
   };
 
   return (
@@ -79,52 +82,49 @@ function DetailNewsPage() {
       ) : (
         <div>
           <div className="col-span-2 px-4 pt-6">
-            <div className="p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 ">
+            <div className="p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2">
               <div className="flow-root">
-                <div className="flow-root">
-                  <div className="mb-8 ">
-                    <h3 className="flex justify-center mb-4 text-xl font-semibold">
-                      {title}
-                    </h3>
-                    <div className="relative w-full h-[500px] flex-shrink-0 mb-4">
-                      <Image
-                        src={
-                          mediaUrl
-                            ? `${process.env.NEXT_PUBLIC_HOST}` + mediaUrl
-                            : LogoNotfound.src
-                        }
-                        width={100}
-                        height={100}
-                        className="object-cover w-full h-full"
-                        alt="Logo Division CCI"
-                      />
-                    </div>
-                    <div className="flex flex-wrap gap-2 justify-between">
-                      {detailsMediaUrl.length > 0 &&
-                        detailsMediaUrl.map((data, i) => (
-                          <div key={i} className="flex-grow">
-                            <Image
-                              src={
-                                mediaUrl
-                                  ? `${process.env.NEXT_PUBLIC_HOST}` +
-                                    data.mediaUri
-                                  : LogoNotfound.src
-                              }
-                              width={100}
-                              height={100}
-                              className="object-cover w-full h-[200px]"
-                              alt="Logo Division CCI"
-                            />
-                          </div>
-                        ))}
-                    </div>
+                <div className="mb-8">
+                  <h3 className="flex justify-center mb-4 text-xl font-semibold">
+                    {title}
+                  </h3>
+                  <div className="relative w-full h-[500px] flex-shrink-0 mb-4">
+                    <Image
+                      src={
+                        mediaUrl
+                          ? `${process.env.NEXT_PUBLIC_HOST}${mediaUrl}`
+                          : LogoNotfound.src
+                      }
+                      width={100}
+                      height={100}
+                      className="object-cover w-full h-full"
+                      alt="News Image"
+                    />
+                  </div>
+                  <div className="flex flex-wrap gap-2 justify-between">
+                    {detailsMediaUrl.map((data, i) => (
+                      <div key={i} className="flex-grow">
+                        <Image
+                          src={
+                            `${process.env.NEXT_PUBLIC_HOST}${data.mediaUri}` ||
+                            LogoNotfound.src
+                          }
+                          width={100}
+                          height={100}
+                          className="object-cover w-full h-[200px]"
+                          alt="Detail Image"
+                        />
+                      </div>
+                    ))}
                   </div>
                 </div>
                 <div className="mb-8">
-                  <Editor
-                    editorState={editorState}
-                    onEditorStateChange={onEditorStateChange}
-                  />
+                  {editorState && (
+                    <EditorNoSSR
+                      editorState={editorState}
+                      onChange={onEditorStateChange}
+                    />
+                  )}
                 </div>
                 <div className="flex items-center">
                   <DefaultLink
